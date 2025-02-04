56dbf21476fdd005aaa1a1bd62430131edb82406
Gustavo Fanha <fanhagustavo@gmail.com>
2025-02-04 15:14:35 -0300

    starting project

---
+++ src/main/java/com/fanha/basic_user_crud/BasicUserCrudApplication.java
@@ -0,0 +1,13 @@
+package com.fanha.basic_user_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicUserCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicUserCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanha/basic_user_crud/BasicUserCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanha.basic_user_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUserCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
